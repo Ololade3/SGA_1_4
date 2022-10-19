4c0329f9e28f5822b881db41ffb3d10da97618fa
unknown <ayoluwatoyin3@gmail.com>
2022-10-19 21:08:41 +0100

    This is my first commit

---
+++ week1_day1/hello_world.py
@@ -0,0 +1,29 @@
+# print("hello world")
+# print("Toyin's world")
+# multiline_strings_ = '''
+# life is beautiful
+# game on
+# python
+# '''
+# print(multiline_strings_)
+
+# my_dict = {"name_1":"telma","age":40,"hobies":["reading","singing","coding"],"my_multi_line":multiline_strings_}
+# print(my_dict
+# )
+# print(my_dict["age"])
+# print(my_dict["hobies"])
+# print(my_dict["my_multi_line"])
+
+# my_list = ["apple","orange","banana"]
+# print(my_list)
+# print(my_list[2])
+
+# # mutating values in a list
+# my_list[2] = "mangoes"
+# print(my_list[2])
+
+my_tuple = ("apple","orange","banana")
+# print(my_tuple)
+# my_tuple[2] = "mangoes"
+print(my_tuple)
+print(type(my_tuple))
---
+++ week1_day2/control_flow.py
@@ -0,0 +1,29 @@
+#If statment practice
+
+x = int(input("please input your grade:"))
+if type(x) == int:
+
+
+    if x >= 70:
+        print("A")
+    elif x>= 60 and x <=69:
+      print("B")
+    elif x>=50 and x <=59:
+     print("C")
+    elif x>=40 and x <=49:
+       print("E")
+    else:
+     print("Failed") 
+
+else: 
+    print("Our program only accepts integer values")
+
+print("I will execute either way")
+
+
+
+
+
+
+
+print("I will execute either way")
